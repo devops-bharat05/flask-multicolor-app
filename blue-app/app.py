a31ea208b2bcf1d558605f9b5e2a66082d8492a4
Bharat Bhushan <116905352+devops-bharat05@users.noreply.github.com>
2025-07-04 06:36:30 +0530

    Create app.py

---
+++ blue-app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import socket
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    color = os.environ.get('COLOR', 'blue')
+    html = f"""
+    <html><body style='background-color:{color}; text-align:center;'>
+    <h1>{color.upper()} App</h1><p>Pod: {hostname}</p><p>IP: {ip_address}</p></body></html>
+    """
+    return html
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
